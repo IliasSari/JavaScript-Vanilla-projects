d4757c1025161f4dfda51b9a93beba5fd0b86fb8
IliasSari <sarigilias@gmail.com>
2026-08-29 18:13:25 +0300

    Created new html,css,js files to make the project perfectgit add .

---
+++ script.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    title: "Color Flip",
+    path: "./Color flip/index.html",
+  },
+  {
+    title: "Counter",
+    path: "./Counter/index.html",
+  },
+  {
+    title: "Reviews",
+    path: "./Reviews/index.html",
+  }
+
+];
+
+const container = document.getElementById("projects-container");
+
+function displayProjects() {
+  container.innerHTML = projects.map(project => {
+    return `
+      <a href="${project.path}" class="card">
+        <div class="icon"><i class="fa-solid ${project.icon}"></i></div>
+        <h2>${project.title}</h2>
+        <p>${project.description}</p>
+      </a>
+    `;
+  }).join("");
+}
+
+displayProjects();
